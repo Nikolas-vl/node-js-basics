560a79c3070bd26d28cc59581069294655b21d2f
Nikolas-vl <nikvv02@gmail.com>
2025-06-10 18:24:02 +0200

    errorHandler

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
-// src/server.js
-
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-
 import { getEnvVar } from './utils/getEnvVar.js';
 import router from './routers/students.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -31,18 +30,9 @@ export const startServer = () => {
 
   app.use(router); // Додаємо роутер до app як middleware
 
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use(notFoundHandler);
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
