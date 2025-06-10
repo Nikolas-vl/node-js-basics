6aad714e2130e6ded10515a013c4a3edf17d86c7
Nikolas-vl <nikvv02@gmail.com>
2025-06-10 18:37:06 +0200

    3 module part 1 errors

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -1,4 +1,5 @@
 import { getAllStudents, getStudentById } from '../services/students.js';
+import createHttpError from 'http-errors';
 
 export const getStudentsController = async (req, res) => {
   const students = await getAllStudents();
@@ -12,20 +13,11 @@ export const getStudentsController = async (req, res) => {
 
 export const getStudentByIdController = async (req, res, next) => {
   const { studentId } = req.params;
+
   const student = await getStudentById(studentId);
 
-  // Код який був до цього
-  // if (!student) {
-  //   res.status(404).json({
-  //     message: "Student not found",
-  //   });
-  //   return;
-  // }
-
-  // А тепер додаємо базову обробку помилки замість res.status(404)
   if (!student) {
-    next(new Error('Student not found'));
-    return;
+    throw createHttpError(404, 'Student not found');
   }
 
   res.json({
